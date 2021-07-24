6b5535609e31275cc4324139fa3678965733526d
Rus Petruta Emilia <rus.petrutaemilia912@gmail.com>
2021-07-24 18:12:40 +0300

    Added project to Github

---
+++ angular-ecommerce/src/app/validators/retroveh-shop-validators.spec.ts
@@ -0,0 +1,7 @@
+import { RetrovehShopValidators } from './retroveh-shop-validators';
+
+describe('RetrovehShopValidators', () => {
+  it('should create an instance', () => {
+    expect(new RetrovehShopValidators()).toBeTruthy();
+  });
+});
